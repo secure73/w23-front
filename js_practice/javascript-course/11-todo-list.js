ebfb87d31631a4157f49703abf88a1c9f7662729
Alex B <alexander.bar@icloud.com>
2023-05-03 13:50:40 +0200

    arrays & loops, accumulator pattern, starting ToDoList-Project

---
+++ js_practice/javascript-course/11-todo-list.js
@@ -0,0 +1,20 @@
+const todoList = ['make dinner', 'wash dishes'];
+
+let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++) {
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`;
+  todoListHTML += html;
+}
+console.log(todoListHTML);
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+  
+  todoList.push(name);
+  console.log(todoList);
+
+  inputElement.value = '';
+}
