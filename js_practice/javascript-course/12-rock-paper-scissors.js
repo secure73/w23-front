07fc8810c08ccf698f0cc79ac3a382cb436eeb50
Alex B <alexander.bar@icloud.com>
2023-05-04 14:10:01 +0200

    added eventlisteners to projects, finished Java-Course Part 1

--- js_practice/javascript-course/12-rock-paper-scissors.js
+++ js_practice/javascript-course/12-rock-paper-scissors.js
@@ -37,6 +37,16 @@ document.querySelector('.js-scissors-button')
   playGame('scissors');
 });
 
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+});
+
 function playGame(playerMove) {
 const computerMove = pickComputerMove();
 
