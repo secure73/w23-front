3cc3402dacc2043f52906513860b44107faaba6c
AbdulakovTokhir <tokhir28@gmail.com>
2023-05-08 03:55:52 +0200

    Update

--- Projekt2/script.js
+++ Projekt2/script.js
@@ -5,22 +5,22 @@
     const message = form.elements["message"];
 
     form.addEventListener("submit", function(event) {
-      event.preventDefault(); // Отменяем отправку формы по умолчанию
+      event.preventDefault(); // Stopp das Standardverhalten des Formulars
 
-      // Проверяем заполненность полей
+      // Überprüfen Sie, ob alle Felder ausgefüllt sind
       if (!name.value || !email.value || !message.value) {
-        alert("Пожалуйста, заполните все поля");
+        alert("AAAAAA, bitte füllen Sie alle Felder aus");
         return;
       }
 
-      // Отправляем данные формы на сервер
+      // Senden Sie die Formulardaten an den Server
       const xhr = new XMLHttpRequest();
       xhr.open("POST", "url_to_server_script.php");
       xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
       xhr.onreadystatechange = function() {
         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-          alert("Сообщение отправлено");
-          form.reset(); // Очищаем форму
+          alert("Nachricht gesendet");
+          form.reset(); // Leeren Sie das Formular
         }
       };
       xhr.send("name=" + name.value + "&email=" + email.value + "&message=" + message.value);
